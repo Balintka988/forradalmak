3459c619dd14114d299225352d24a7b8cf1e866d
Szikora Balint <balint.szikora00@gmail.com>
2025-04-04 22:55:58 +0200

    tablazat hozzadasa sima

--- main.js
+++ main.js
@@ -8,6 +8,26 @@ const containerDiv = makeDiv('container'); // container nevű divet csinál
 document.body.appendChild(containerDiv); // hozzáadja a bodyhoz a container divet
 const tableDiv = makeDiv('table'); // csinál egy table nevű divet
 
+const tableSim = document.createElement('table'); // csinál egy table elemet
+tableDiv.appendChild(tableSim); // belerakja a tableDiv-be (amit korábban csináltunk)
+
+const tableHead = document.createElement('thead'); // létrehozza a fejléc részt
+tableSim.appendChild(tableHead); // hozzáadja a táblázathoz a fejlécet
+
+const tableHeadRow = document.createElement('tr'); // egy sor a thead részbe
+tableHead.appendChild(tableHeadRow); // hozzáadjuk a sort a thead-hez
+
+const theadCells = ['forradalom', 'evszam', 'sikeres']; // a fejléc cellák tartalma, tömbbe tároljuk el
+for (const cellContent of theadCells) { // végigmegyünk a tömb elemein
+    const thcell = document.createElement('th'); // csinálunk egy új th elemet
+    thcell.innerText = cellContent; // beleírjuk a cellába az aktuális elemet
+    tableHeadRow.appendChild(thcell); // hozzáadjuk a cellát a fejléchez
+}
+
+const tbody = document.createElement('tbody'); // létrehozzuk a table body részét
+tableSim.appendChild(tbody); // berakjuk a table-be
+
+
 const formDiv = makeDiv('form'); // form divet is csinálunk
 
 containerDiv.appendChild(tableDiv); // belerakjuk a table divet a containerbe
